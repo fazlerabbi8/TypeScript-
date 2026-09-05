a040c74409ca164a8c3441a96e4037cd5ab2f1aa
fazlerabbi13 <mohammad0132794@gmail.com>
2026-09-05 16:06:57 +0600

    practice types and function annotation.

---
+++ types.js
@@ -0,0 +1,13 @@
+// static type
+let id = 5;
+let userName = "mohamad";
+let isActive = true;
+let tags = ['ts', 'js'];
+let score = [5, 6, 8];
+let mixed = ['rabbi', 23];
+// funtion
+function calculateTotal(price, quantity) {
+    const total = price * quantity;
+    return total;
+}
+export {};
---
+++ types.ts
@@ -0,0 +1,21 @@
+// static type
+let id: number = 5;
+let userName: string = "mohamad";
+let isActive: boolean = true;
+let tags: string[] = ['ts', 'js'];
+let score: number[] = [5 , 6 , 8];
+let mixed: (string | number)[] = ['rabbi', 23];
+
+
+
+// funtion
+function calculateTotal(price: number, quantity: number) : number {
+    const total = price * quantity;
+    return total;
+}
+
+
+
+
+export {}
+
